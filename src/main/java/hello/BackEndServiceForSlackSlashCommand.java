9ac04a5fbf992ca94142bb885f61f49000df8ec8
pboopathi <pboopathi@ebay.com>
2020-08-10 13:18:32 -0700

    Fixed slf4j: Failed to load class org.slf4j.impl.StaticLoggerBinder

--- src/main/java/hello/BackEndServiceForSlackSlashCommand.java
+++ src/main/java/hello/BackEndServiceForSlackSlashCommand.java
@@ -4,7 +4,7 @@ import com.slack.api.bolt.App;
 import com.slack.api.bolt.AppConfig;
 import com.slack.api.bolt.jetty.SlackAppServer;
 
-public class MyApp {
+public class BackEndServiceForSlackSlashCommand {
     public static  void main(String[] args) throws Exception {
 
         AppConfig appConfig = new AppConfig();
